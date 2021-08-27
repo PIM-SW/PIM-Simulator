d359de5ad607c6c5da71f11ee25a985358107b55
hyoseongCHOI <hyoseong.CHOI@yonsei.ac.kr>
2021-08-27 07:26:14 +0000

    Nothing

--- src/cblas_sdot.c
+++
@@ -1,8 +0,0 @@
-#include "pim_avail_op.h"
-
-float cblas_sdot(const int N, const float  *X, const int incX,
-                  const float  *Y, const int incY) 
-{
-    printf("NOT YET\n");
-    return 0;
-}
--- src/cblas_sdsdot.c
+++
@@ -1,8 +0,0 @@
-#include "pim_avail_op.h"
-
-float cblas_sdsdot(const int N, const float alpha, const float *X,
-                    const int incX, const float *Y, const int incY)
-{
-    printf("NOT YET\n");
-    return 0;
-}
